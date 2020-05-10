cf876fdecbd48260a9f3109ce642e713c4eb7254
Abdullahi <abdullahihassanke@gmail.com>
2020-05-10 19:40:25 +0300

    intro-page-finish

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,13 @@ class LandingPage extends Component {
   render() {
     return (
       <main>
-        <h1>LandingPage</h1>
+        <div className="skew">
+          <div className="content">
+            <h1>Mental Remedy</h1>
+            <p>linking mental health specialist to the community.</p>
+            <input type="text" name="box" placeholder="Search For Specialists...." />
+          </div>
+        </div>
       </main>
     );
   }
