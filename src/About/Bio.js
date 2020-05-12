3c2385750d302c94766ba32f0156b881f68169a5
Abdullahi <abdullahihassanke@gmail.com>
2020-05-12 19:16:57 +0300

    Bio-state

--- src/About/Bio.js
+++ src/About/Bio.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Bio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info:
+        'Due to the Stigma and scrutiny associated with mental illnesses, individuals who notice change in behavioural attitude may desist from seeking professional help cause of this stigma. Our main goal is to educate and establish effective means of communication between specialists and the community.'
+    };
+  }
+
   render() {
     return (
       <div className="about">
         <h1 className="sub-heading">About Us</h1>
-        <p className="medium-text">
-          Due to the Stigma and scrutiny associated with mental illnesses, individuals who notice change in behavioural
-          attitude may desist from seeking professional help cause of this stigma. Our main goal is to educate and
-          establish effective means of communication between specialists and the community.
-        </p>
+        <p className="medium-text">{this.state.info}</p>
       </div>
     );
   }
