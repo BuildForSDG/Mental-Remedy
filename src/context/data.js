d7bf229b27c9fa07d602831671b1d725c620473a
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-05-13 19:59:04 +0300

    sample data

---
+++ src/context/data.js
@@ -0,0 +1,19 @@
+const mentalDisorders = [
+  {
+    id: 101,
+    title: 'Anxiety disorder',
+    description: 'A mental health disorder characterised by feelings of worry, anxiety or fear that are strong enough to interfere with one\'s daily activities.',
+    diagnosis: 'Usually self-diagnosable',
+    symptoms: 'Symptoms include stress that\'s out of proportion to the impact of the event, inability to set aside a worry and restlessness.',
+    treatment: 'Treatment includes counselling or medication, including antidepressants.'
+  },
+  {
+    id: 101,
+    title: 'Clinical depression',
+    description: 'Also known as major depression, a mental health disorder characterised by persistently depressed mood or loss of interest in activities, causing significant impairment in daily life. The persistent feeling of sadness or loss of interest that characterises major depression can lead to a range of behavioural and physical symptoms. Depression can also be associated with thoughts of suicide. Depression can also be associated with thoughts of suicide.',
+    symptoms: 'Symptoms include stress that\'s out of proportion to the impact of the event, inability to set aside a worry and restlessness.',
+    treatment: 'The mainstay of treatment is usually medication, talk therapy or a combination of the two. I'
+  }
+];
+
+export default mentalDisorders;
