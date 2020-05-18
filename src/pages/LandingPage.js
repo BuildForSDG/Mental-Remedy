ce6715f1c9d0b557161bde6913412f5b6991718b
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-05-18 15:25:03 +0300

    landing page fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import IntroHeader from '../intro/IntroHeader';
 import About from '../About/About';
-<<<<<<< HEAD
 import MdPreview from '../mdList-preview/MdPreview';
-=======
 import SpecialistsPrev from '../specialists/SpecialistsPrev';
->>>>>>> 6f43e2ffee92547125f472b68cbebd2f2851ad7d
 
 class LandingPage extends Component {
   render() {
@@ -13,11 +10,8 @@ class LandingPage extends Component {
       <main>
         <IntroHeader />
         <About />
-<<<<<<< HEAD
         <MdPreview />
-=======
         <SpecialistsPrev />
->>>>>>> 6f43e2ffee92547125f472b68cbebd2f2851ad7d
       </main>
     );
   }
