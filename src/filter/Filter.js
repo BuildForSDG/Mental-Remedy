c6515bea6462c97ec0e5dca78855b29014844210
Abdullahi <abdullahihassanke@gmail.com>
2020-05-29 18:37:57 +0300

    fix: make requested changes

--- src/filter/Filter.js
+++ src/filter/Filter.js
@@ -6,12 +6,7 @@ export default class Filter extends Component {
       <div>
         <form>
           <h1>Find the Best Specialist</h1>
-          <input
-            type="text"
-            className="search-field name"
-            placeholder="Name..."
-            onChange={(event) => this.setState({ name: event.target.value })}
-          />
+          <input type="text" className="search-field name" placeholder="Name..." />
           <input type="text" className="search-field city" placeholder="City..." />
         </form>
       </div>
