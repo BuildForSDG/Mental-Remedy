e0b757190cbe5f1abc677361577486cf42cff274
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-05-29 16:49:30 +0300

    fix: add eslint rule to allow decontruction

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,6 +52,18 @@ module.exports = {
     'multiline-ternary': 0,
     'no-magic-numbers': 0,
     'no-return-assign': 0,
+    "prefer-destructuring": ["error", {
+      "VariableDeclarator": {
+        "array": false,
+        "object": true
+      },
+      "AssignmentExpression": {
+        "array": true,
+        "object": false
+      }
+    }, {
+      "enforceForRenamedProperties": false
+    }],
     'sort-imports': [
       'error',
       {
