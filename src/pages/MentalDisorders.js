a003cf1b17ee389bb07739f6c7d4eeaf2859e257
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-05-31 11:39:13 +0300

    feat: create MentalDisorders page and set up its route

---
+++ src/pages/MentalDisorders.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MentalDisorders extends Component {
+  render() {
+    return (
+            <main>
+                <h1 className="heading">Mental Health</h1>
+            </main>
+    );
+  }
+}
