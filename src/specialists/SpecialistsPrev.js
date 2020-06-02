5607476f2b159c9a71cb610514fdbb7b3721ec81
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-02 22:45:17 +0300

    fix: fix responsiveness and add scroll to top added scroll to top function to all pages fixed margin for footer and services

--- src/specialists/SpecialistsPrev.js
+++ src/specialists/SpecialistsPrev.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { MdFindReplace } from 'react-icons/md';
 import { Consumer } from '../context/Context';
 import SpecialistsLists from './SpecialistsLists';
 
@@ -10,7 +11,7 @@ export default class SpecialistsPrev extends Component {
           const { getSpecialists, specialists, startSlider } = value;
           return (
             <div className="preview" onLoad={() => startSlider()}>
-              <h1 className="sub-heading">Specialists Preview</h1>
+              <h1 className="heading">Specialists Preview</h1>
               <hr />
               <div className="swiper-container">
                 <SpecialistsLists specialists={specialists}
@@ -19,6 +20,7 @@ export default class SpecialistsPrev extends Component {
                 <div className="swiper-button-next"></div>
                 <div className="swiper-button-prev"></div>
               </div>
+              <button className="small-heading large-btn"><MdFindReplace/> Browse All</button>
             </div>
           );
         }}
