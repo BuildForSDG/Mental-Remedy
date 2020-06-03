7615e11313dfe8e5c16a902cd0ed7abffbd78308
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-03 11:41:03 +0300

    feat: add breadcrumbs in SingleMd component

--- src/single-md/SingleMd.js
+++ src/single-md/SingleMd.js
@@ -5,6 +5,9 @@ export default class SingleMd extends Component {
     return (
             <div className="lg-container">
                 <h1 className="sub-heading">{this.props.md.title}</h1>
+                <div className="breadcrumbs">
+                    <span className="xxsmall-text"><a href="/">Mental Disorders</a> / {this.props.md.title}</span>
+                </div>
             </div>
     );
   }
