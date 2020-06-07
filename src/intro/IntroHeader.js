a32c5b512ed3746299d8d3d9a754c37a16d93502
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-07 08:48:00 +0300

    feat: add logo

--- src/intro/IntroHeader.js
+++ src/intro/IntroHeader.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import logo from '../favicon_io/android-chrome-512x512.png';
 
 export default class IntroHeader extends Component {
   render() {
     return (
       <div className="skew">
         <div className="content">
-          <h1 className="heading">Mental Remedy</h1>
+          <h1 className="heading">Mental <img src={logo} alt="logo" className="logo" /> Remedy</h1>
           <p className="small-text">Linking mental health specialist to the community.</p>
           <input
             className="intro-search small-text"
