b93905535b42d8dd40a72f1c6e90a7e269fd8e39
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-09 11:57:53 +0300

    fix: move reducers to Reducers.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcSBkYLu9aLqMhROD3f3Y9c-DlcvRdTP4',
+  authDomain: 'mental-remedy.firebaseapp.com',
+  databaseURL: 'https://mental-remedy.firebaseio.com',
+  projectId: 'mental-remedy',
+  storageBucket: 'mental-remedy.appspot.com',
+  messagingSenderId: '85742469048',
+  appId: '1:85742469048:web:7a9b49a93d72a8845d194d',
+  measurementId: 'G-4GKCSPWS9S'
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
