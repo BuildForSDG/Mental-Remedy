3c0142d8440fe9f259a1f068b978fb3b233a0d8f
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-10 10:00:03 +0300

    fix: remove search bar from intro header

--- src/intro/IntroHeader.js
+++ src/intro/IntroHeader.js
@@ -1,21 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import logo from '../favicon_io/android-chrome-512x512.png';
 
-export default class IntroHeader extends Component {
+class IntroHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.mentalHealth = this.mentalHealth.bind(this);
+    this.specialists = this.specialists.bind(this);
+  }
+
+  mentalHealth(event) {
+    event.preventDefault();
+    this.props.history.push('/mental-disorders');
+  }
+
+  specialists(event) {
+    event.preventDefault();
+    this.props.history.push('/specialists');
+  }
+
   render() {
     return (
       <div className="skew">
         <div className="content">
           <h1 className="heading">Mental <img src={logo} alt="logo" className="logo" /> Remedy</h1>
-          <p className="small-text">Linking mental health specialist to the community.</p>
-          <input
-            className="intro-search small-text"
-            type="text"
-            name="box"
-            placeholder="mental disorders, specialists...."
-          />
+          <p className="small-text">"Mistaken beliefs that mental health disorders cannot be treated, ignorance about the extent of mental health problems and stigma against those living with mental health disorders are the major issues that undermine any efforts made to enhance mental health treatment and awareness. In September 2015 the United Nations General Assembly included mental health and substance abuse in the global Sustainable Development Goals, marking the first time world leaders recognised mental health as a global priority."</p>
+
+          <button className="small-heading large-btn"
+          onClick={this.specialists}>Find Specialists</button>
+          <button className="small-heading large-btn"
+          onClick={this.mentalHealth}>Mental Health</button>
+
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(IntroHeader);
