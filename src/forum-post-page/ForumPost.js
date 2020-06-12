61c85fc0b1471cca021580414ccf0f519bd43d2f
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-06-12 08:43:40 +0300

    feat: add function for deleting a forum post

--- src/forum-post-page/ForumPost.js
+++ src/forum-post-page/ForumPost.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Comment from '../forum/Comment';
 import PostOptions from '../forum/PostOptions';
-import img from '../context/images/male.svg';
 import CommentModal from './CommentModal';
 import { Consumer } from '../context/Context';
+import PostDetails from './PostDetails';
 
 export default class ForumPost extends Component {
   componentDidMount() {
@@ -16,37 +16,31 @@ export default class ForumPost extends Component {
             <Consumer>
                 {(value) => {
                   return (
-                        <>
-                        <div className="post">
-                    <img src={img} alt={this.props.historyLocation.profile.username} />
-                    <div>
-                        <h5 className="xxsmall-text">{this.props.historyLocation.profile.username}</h5>
-                        <p className="small-text">{this.props.historyLocation.forumPost.post}</p>
-                    </div>
-                </div>
-                <CommentModal historyLocation={this.props.historyLocation} />
-                <div className="comments">
-                    {value.comments.map((key) => (
-                        <Comment
-                            key={key.id}
-                            comment={key}
-                            profiles={value.profiles}
-                            user={this.props.historyLocation.user}
-                            forumPost={this.props.historyLocation.forumPost}
-                            getComments={value.getComments}
-                            />
-                    ))}
-                </div>
-                <PostOptions
-                    forumPost={this.props.historyLocation.forumPost}
-                    comments={this.props.historyLocation.comments}
-                    user={this.props.historyLocation.user}
-                    deleteOption={true}
-                    addComment={true}
-                    goToComments={() => null}
-                    goToCommentForm={() => value.dispatch({ type: 'TOGGLECOMMENTFORM', payload: true })}
-                    />
-                        </>
+                    <>
+                      <PostDetails historyLocation={this.props.historyLocation} />
+                      <PostOptions
+                          forumPost={this.props.historyLocation.forumPost}
+                          comments={this.props.historyLocation.comments}
+                          user={this.props.historyLocation.user}
+                          deleteOption={true}
+                          addComment={true}
+                          goToComments={() => null}
+                          goToCommentForm={() => value.dispatch({ type: 'TOGGLECOMMENTFORM', payload: true })}
+                          />
+                      <CommentModal historyLocation={this.props.historyLocation} />
+                      <div className="comments">
+                          {value.comments.map((key) => (
+                              <Comment
+                                  key={key.id}
+                                  comment={key}
+                                  profiles={value.profiles}
+                                  user={this.props.historyLocation.user}
+                                  forumPost={this.props.historyLocation.forumPost}
+                                  getComments={value.getComments}
+                                  />
+                          ))}
+                      </div>
+                    </>
                   );
                 }}
             </Consumer>
